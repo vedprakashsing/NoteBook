347bf08732d34fcdb069ab23ce064382e00cae04
Sanskar Agrawal <sanskar10100@gmail.com>
2022-01-14 00:15:55 +0530

    Add login by Email

--- app/src/main/java/io/app/notebook/data/Data.java
+++ app/src/main/java/io/app/notebook/data/Data.java
@@ -4,6 +4,9 @@ public class Data {
     private static Data INSTANCE = null;
     public AppDatabase db;
 
+    // This is the id of the current user. Use this to create and fetch notes.
+    public long uid;
+
     public static Data getInstance() {
         if (INSTANCE == null) {
             INSTANCE = new Data();
